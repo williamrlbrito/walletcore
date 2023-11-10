44e036acb96615a90c947b935b16ca2a54bf8b94
William Brito <williamrldb@gmail.com>
2023-11-10 14:10:00 -0400

    --Preparando suite de testes

---
+++ internal/database/client_db_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type ClientDBTestSuite struct {
+	suite.Suite
+	db       *sql.DB
+	ClientDB *ClientDB
+}
+
+func (suite *ClientDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	suite.Nil(err)
+	suite.db = db
+	db.Exec("CREATE TABLE clients (id TEXT, name TEXT, email TEXT, created_at DATETIME)")
+	suite.ClientDB = NewClientDB(db)
+}
+
+func (suite *ClientDBTestSuite) TearDownSuite() {
+	defer suite.db.Close()
+	suite.db.Exec("DROP TABLE clients")
+}
+
+func TestClientDBTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientDBTestSuite))
+}
