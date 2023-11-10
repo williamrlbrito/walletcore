1b63932ae7d7fe755049a25a94169d739ebf629f
William Brito <williamrldb@gmail.com>
2023-11-09 21:57:54 -0400

    --Testando caso de uso

---
+++ internal/usecase/create_client/create_client_test.go
@@ -0,0 +1,43 @@
+package createclient
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/williamrlbrito/walletcore/internal/entity"
+)
+
+type ClientGatewayMock struct {
+	mock.Mock
+}
+
+func (mock *ClientGatewayMock) Get(id string) (*entity.Client, error) {
+	args := mock.Called(id)
+	return args.Get(0).(*entity.Client), args.Error(1)
+}
+
+func (mock *ClientGatewayMock) Save(client *entity.Client) error {
+	args := mock.Called(client)
+	return args.Error(0)
+}
+
+func TestCreateClientUseCase(t *testing.T) {
+	clientGatewayMock := &ClientGatewayMock{}
+	clientGatewayMock.On("Save", mock.Anything).Return(nil)
+
+	useCase := NewCreateClientUseCase(clientGatewayMock)
+
+	output, err := useCase.Execute(&CreateClientInputDTO{
+		Name:  "Jhon Doe",
+		Email: "j@j",
+	})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.NotEmpty(t, output.ID)
+	assert.Equal(t, "Jhon Doe", output.Name)
+	assert.Equal(t, "j@j", output.Email)
+	clientGatewayMock.AssertExpectations(t)
+	clientGatewayMock.AssertNumberOfCalls(t, "Save", 1)
+}
