77235856e24d00e2df41e98f49f3194931d2ef29
William Brito <williamrldb@gmail.com>
2023-11-10 09:33:35 -0400

    --Criando CreateAccountUseCase

---
+++ internal/gateway/account.go
@@ -0,0 +1,8 @@
+package gateway
+
+import "github.com/williamrlbrito/walletcore/internal/entity"
+
+type AccountGateway interface {
+	Save(account *entity.Account) error
+	FindById(id string) (*entity.Account, error)
+}
---
+++ internal/usecase/create_account/create_account.go
@@ -0,0 +1,44 @@
+package createaccount
+
+import (
+	"github.com/williamrlbrito/walletcore/internal/entity"
+	"github.com/williamrlbrito/walletcore/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientId string
+}
+
+type CreateAccountOutputDTO struct {
+	Id string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(accountGateway gateway.AccountGateway, clientGateway gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: accountGateway,
+		ClientGateway:  clientGateway,
+	}
+}
+
+func (useCase *CreateAccountUseCase) Execute(input *CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := useCase.ClientGateway.Get(input.ClientId)
+	if err != nil {
+		return nil, err
+	}
+
+	account := entity.NewAccount(client)
+
+	err = useCase.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutputDTO{
+		Id: account.ID,
+	}, nil
+}
